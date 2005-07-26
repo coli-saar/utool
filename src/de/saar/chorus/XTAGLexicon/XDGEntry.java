0b0874876ddb7fcccc1eff4faf7211e751f83b8b
koller <koller@localhost>
2005-07-26 15:01:20 +0000

    indentation

--- src/de/saar/chorus/XTAGLexicon/XDGEntry.java
+++ src/de/saar/chorus/XTAGLexicon/XDGEntry.java
@@ -3,25 +3,25 @@ package de.saar.chorus.XTAGLexicon;
 import java.util.*;
 
 public class XDGEntry {
-
-    public List<String> outId;
-    public Set<String> inLp;
-    public List<String> outLp;
-    public String anchor;
-    public String anchorAddress;
-    public Map<String, Set<String>> linking;
-    public String rootCat;
-    public int number;
-    public String auxDirection = "";
-    public boolean isAux = false;
-    public boolean passedFoot = false;
-
-    public XDGEntry(int num){
-	outId = new ArrayList<String>();
-	outLp = new ArrayList<String>();
-	inLp = new HashSet<String>();
-	linking = new HashMap<String, Set<String>>();
-	number = num;
-    }
-
+	
+	public List<String> outId;
+	public Set<String> inLp;
+	public List<String> outLp;
+	public String anchor;
+	public String anchorAddress;
+	public Map<String, Set<String>> linking;
+	public String rootCat;
+	public int number;
+	public String auxDirection = "";
+	public boolean isAux = false;
+	public boolean passedFoot = false;
+	
+	public XDGEntry(int num){
+		outId = new ArrayList<String>();
+		outLp = new ArrayList<String>();
+		inLp = new HashSet<String>();
+		linking = new HashMap<String, Set<String>>();
+		number = num;
+	}
+	
 }
