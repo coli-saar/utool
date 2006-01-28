512644e224f2f6be961a804893d58536c7c14da5
koller <koller@localhost>
2006-01-28 23:15:46 +0000

    Help texts.

--- src/de/saar/chorus/domgraph/utool/Utool.java
+++ src/de/saar/chorus/domgraph/utool/Utool.java
@@ -11,7 +11,6 @@ import java.io.FileWriter;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.List;
-import java.util.Set;
 
 import de.saar.chorus.domgraph.chart.Chart;
 import de.saar.chorus.domgraph.chart.ChartSolver;
@@ -41,11 +40,92 @@ public class Utool {
     private static final char OPTION_DUMP_CHART = (char) 3;
     
     private enum Operation {
-        solve,
-        convert,
-        help,
-        solvable,
-        classify
+        solve      
+        ("Solve an underspecified description",
+                "Usage: utool solve [options] [input-source]\n\n" +
+                "By default, reads an underspecified description from standard input\n" +
+                "and writes the solutions to standard output. An alternative input source\n" +
+                "can be specified on the command line; an alternative filename for the\n" +
+                "output can be specified with the -o option. The input and output codecs\n" +
+                "can be specified with the -I and -O options. If only an input codec\n" +
+                "is specified, and an output codec of the same name exists, this codec\n" +
+                "is used for output. `utool --display-codecs' will display a list of\n" +
+                "input and output codecs.\n\n" +
+                "Valid options:\n" +
+                "  --output, -o filename           Write solutions to a file.\n" +
+                "  --input-codec, -I codecname     Specify the input codec.\n" +
+                "  --output-codec, -O codecname    Specify the output codec (default: same as input).\n"),
+                
+        solvable   
+        ("Check solvability without enumerating solutions",
+                "Usage: utool solvable [options] [input-source]\n\n" +
+                "This command checks whether an underspecified description is solvable.\n" +
+                "If it is, utool terminates with an exit code of 0; if it isn't, it terminates\n" +
+                "with an exit code of 1.\n\n" +
+                "The \'solvable\' command computes the total number of solved forms (= readings),\n" +
+                "but not the solved forms themselves (use \'solve\' if you want them). This makes\n" +
+                "\'solvable\' run much, much faster than \'solve\'. utool will display the total\n" +
+                "number of solved forms if you run \'utool solvable -s\'.\n\n" +
+                "By default, reads an underspecified description from standard input\n" +
+                "and writes the solutions to standard output. An alternative input source\n" +
+                "can be specified on the command line; an alternative filename for the\n" +
+                "output can be specified with the -o option. The input codec\n" +
+                "can be specified with the -I option. `utool --display-codecs'\n" +
+                "will display a list of valid input codecs.\n\n" +
+                "Valid options:\n" +
+                "  --input-codec, -I codecname     Specify the input codec.\n"),
+                
+                
+        convert    
+        ("Convert underspecified description from one format to another",
+                "Usage: utool convert -I inputcodec -O outputcodec [options] [input-source]\n\n" +
+                "By default, reads an underspecified description from standard input\n" +
+                "and writes it (in a different format) to standard output. An alternative\n" +
+                "input source can be specified on the command line; an alternative filename\n" +
+                "for the output can be specified with the -o option. The input and output\n" +
+                "codecs can be specifieid with the -I and -O options. `utool --display-codecs'\n" +
+                "will display a list of input and output codecs.\n\n" +
+                "Valid options:\n" +
+                "  --output, -o filename           Write solutions to a file.\n" +
+                "  --input-codec, -I codecname     Specify the input codec.\n" +
+                "  --output-codec, -O codecname    Specify the output codec (default: same as input).\n"),
+
+                
+        classify   
+        ("Check whether a description belongs to special classes",
+                "Usage: utool classify [options] [input-source]\n\n" +
+                "This command checks whether an underspecified description belongs to a\n" +
+                "class with special properties. A call to \'utool classify\' returns an\n" +
+                "exit code that is the OR combination of some of the following values:\n\n" +
+                "    1   the description is a weakly normal dominance graph\n" +
+                "    2   the description is a normal dominance graph\n" +
+                "    4   the description is compact\n" +
+                "    8   the description can be compactified (or is already compact)\n" +
+                "   16   the description is hypernormally connected\n" +
+                "   32   the description is leaf-labelled\n\n" +
+                "For example, the exit code for a graph that is hypernormally connected\n" +
+                "and normal (and hence compactifiable), but not compact, would be 27.\n\n" +
+                "Note that the notion of hypernormal connectedness only makes sense\n" +
+                "for normal graphs (although utool will test for it anyway).\n\n" +
+                "Valid options:\n" +
+                "  --input-codec, -I codecname     Specify the input codec.\n"),                
+                
+        help       
+        ("Display help on a command",
+                "Usage: utool help [command]\n\n" +
+                "Without any further parameters, \'utool help\' displays a list of available\n" +
+                "commands. Alternatively, pass one of the command names to \'utool help\' as the\n" +
+                "second parameter to get command-specific help for this command.\n")
+
+                ;
+
+        
+        public String shortDescription, longDescription;
+        
+        Operation(String shortDescription, String longDescription) {
+            this.shortDescription = shortDescription;
+            this.longDescription = longDescription;
+        }
     }
 
     /**
@@ -55,13 +135,12 @@ public class Utool {
         InputCodec inputCodec = null;
         OutputCodec outputCodec = null;
         Operation op = null;
-        String inputsrc = null;
+        String argument = null;
         Writer output = new OutputStreamWriter(System.out);
         String outputname;
         boolean displayStatistics = false;
         boolean noOutput = false;
         DomGraph graph = new DomGraph();
-        // TODO compactification
         NodeLabels labels = new NodeLabels();
         boolean dumpChart = false;
         
@@ -81,7 +160,7 @@ public class Utool {
         }
         
         if( rest.size() > 1 ) {
-            inputsrc = rest.get(1);
+            argument = rest.get(1);
         }
         
         
@@ -91,21 +170,32 @@ public class Utool {
             System.exit(0);
         }
         
-        if( getopt.hasOption('h')) {
+        if( getopt.hasOption('h') ) {
             displayHelp(op);
             System.exit(0);
         }
         
+        if( op == Operation.help ) {
+            displayHelp(resolveOperation(argument));
+            System.exit(0);
+        }
+        
+        if( getopt.hasOption(OPTION_HELP_OPTIONS)) {
+            displayHelpOptions();
+            System.exit(0);
+        }
+
         if( getopt.hasOption(OPTION_VERSION)) {
             displayVersion();
             System.exit(0);
         }
         
         
+        
         // at this point, we must have an operation
         if( op == null ) {
-            System.err.println("You must specify an operation!");
-            System.exit(1);
+            displayHelp(null);
+            System.exit(0);
         }
         
         
@@ -119,8 +209,8 @@ public class Utool {
         }
         
         if( inputCodec == null ) {
-            if( inputsrc != null ) {
-                inputCodec = codecManager.getInputCodecForFilename(inputsrc);
+            if( argument != null ) {
+                inputCodec = codecManager.getInputCodecForFilename(argument);
             }
         }
         
@@ -161,7 +251,7 @@ public class Utool {
         
         // obtain graph
         try {
-            inputCodec.decode(inputsrc, graph, labels);
+            inputCodec.decode(argument, graph, labels);
         } catch(Exception e) {
             e.printStackTrace();
             System.exit(1);
@@ -361,12 +451,8 @@ public class Utool {
             
         case help:
             
-            // TODO implement this
-            
+            // This was handled above.
             break;
-            
-        
-        
         }
         
         
@@ -375,6 +461,9 @@ public class Utool {
     
     
     
+
+
+
     private static void printChartStatistics(Chart chart, long time, boolean dumpChart) {
         System.err.println("Edges in chart: " + chart.size());
         if( dumpChart ) {
@@ -389,7 +478,24 @@ public class Utool {
 
 
     private static void displayHelp(Operation op) {
-        System.err.println("Placeholder for help on " + op);
+        if( op == null ) {
+            System.err.println("Usage: java -jar Utool.jar <subcommand> [options] [args]");
+            System.err.println("Type `utool help <subcommand>' for help on a specific subcommand.");
+            System.err.println("Type `utool --help-options' for a list of global options.");
+            System.err.println("Type `utool --display-codecs' for a list of supported codecs.\n");
+            
+            System.err.println("Available subcommands:");
+            for( Operation _op : Operation.values() ) {
+                System.err.println(String.format("    %1$-12s %2$s.",
+                        _op, _op.shortDescription));
+            }
+
+            System.err.println("\nutool is the Swiss Army Knife of Underspecification.");
+            System.err.println("For additional information, see http://utool.sourceforge.net/");
+        } else {
+            System.err.println("utool " + op + ": " + op.shortDescription + ".");
+            System.err.println(op.longDescription);
+        }
     }
 
     private static void displayVersion() {
@@ -452,6 +558,18 @@ public class Utool {
                         "Display the chart after solving", null);
         
         return getopt;
+    }
+
+    
+
+    private static void displayHelpOptions() {
+        System.err.println("utool global options are:");
+        System.err.println("  --help-options                    Displays this information about global options.");;
+        System.err.println("  --display-codecs, -d              Displays all input and output filters.");
+        System.err.println("  --display-statistics, -s          Displays runtime and other statistics.");
+        System.err.println("  --no-output, -n                   Do not display computed output.");
+        System.err.println("  --equivalences, -e <filename>     Eliminate equivalent readings.");
+        System.err.println("  --version                         Display version and copyright information.");
     }
 
 }
