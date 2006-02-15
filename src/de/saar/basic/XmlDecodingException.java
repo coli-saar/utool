e1a9ed4daaa93208021d2ba37605bbdd965e3765
koller <koller@localhost>
2006-02-15 07:29:13 +0000

    Extended the XML decoding method to deal with numeric character entities (fixes ticket #19).

---
+++ src/de/saar/basic/XmlDecodingException.java
@@ -0,0 +1,32 @@
+/*
+ * @(#)XmlDecodingException.java created 15.02.2006
+ * 
+ * Copyright (c) 2006 Alexander Koller
+ *  
+ */
+
+package de.saar.basic;
+
+public class XmlDecodingException extends Exception {
+
+    public XmlDecodingException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public XmlDecodingException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public XmlDecodingException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public XmlDecodingException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
