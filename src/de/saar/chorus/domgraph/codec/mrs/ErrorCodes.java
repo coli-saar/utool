2202ac9e24e707cfa07d5644cc04cdc2f91404e8
stth <stth@localhost>
2006-03-03 17:00:36 +0000

    MrsCodec: now reports error codes (ErrorCodes.java)

---
+++ src/de/saar/chorus/domgraph/codec/mrs/ErrorCodes.java
@@ -0,0 +1,10 @@
+package de.saar.chorus.domgraph.codec.mrs;
+
+public class ErrorCodes {
+	 
+    public static final int  NOT_NORMAL = 1;
+    public static final int  NOT_LEAF_LABELLED = 2;
+    public static final int  NOT_HYPERNORMALLY_CONNECTED = 4;
+    
+    public static final int  NOT_WELLFORMED = 16;
+}
