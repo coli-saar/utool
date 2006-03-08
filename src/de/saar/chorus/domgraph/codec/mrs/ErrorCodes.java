8eac916e29e21852622f93aa11cba0d1d1a7120b
stth <stth@localhost>
2006-03-08 17:45:12 +0000

    MrsXmlCodec: works now (tested on Rondane) DomGraph: getRoots checks for cycles, fixed bug in reachable

--- src/de/saar/chorus/domgraph/codec/mrs/ErrorCodes.java
+++ src/de/saar/chorus/domgraph/codec/mrs/ErrorCodes.java
@@ -3,8 +3,9 @@ package de.saar.chorus.domgraph.codec.mrs;
 public class ErrorCodes {
 	 
     public static final int  NOT_NORMAL = 1;
-    public static final int  NOT_LEAF_LABELLED = 2;
-    public static final int  NOT_HYPERNORMALLY_CONNECTED = 4;
+    public static final int  NOT_WEAKLY_NORMAL = 2;
+    public static final int  NOT_LEAF_LABELLED = 4;
+    public static final int  NOT_HYPERNORMALLY_CONNECTED = 8;
     
     public static final int  NOT_WELLFORMED = 16;
 }
--- src/de/saar/chorus/domgraph/codec/mrs/MrsXmlInputCodec.java
+++ src/de/saar/chorus/domgraph/codec/mrs/MrsXmlInputCodec.java
@@ -20,7 +20,7 @@ import de.saar.chorus.domgraph.codec.MalformedDomgraphException;
 import de.saar.chorus.domgraph.codec.ParserException;
 import de.saar.chorus.domgraph.graph.DomGraph;
 import de.saar.chorus.domgraph.graph.NodeLabels;
-
+       
 public class MrsXmlInputCodec extends InputCodec {
 	
 	private MrsCodec codec;
@@ -42,6 +42,7 @@ public class MrsXmlInputCodec extends InputCodec {
 	
 	private class XmlParser extends DefaultHandler {
 		
+		private StringBuilder chars;
 		private Stack<String> parents;
 		private Map<String,String> attrs;
 		private String attr;
@@ -55,6 +56,7 @@ public class MrsXmlInputCodec extends InputCodec {
 		public XmlParser()
 		{
 			super();
+			this.chars = new StringBuilder();
 			this.parents = new Stack<String>();
 			this.attrs = new TreeMap<String,String>();
 			this.top = "";
@@ -71,15 +73,19 @@ public class MrsXmlInputCodec extends InputCodec {
 				String vid = attributes.getValue("vid");
 				String parent = parents.peek();
 				
-				try {
-					if (vid.startsWith("x")) {
-						codec.tellVariable(vid);
-					} else if (vid.startsWith("h")) {
-						codec.tellHandle(vid);
+				if (vid.startsWith("x")) {
+					try {
+						codec.tellVariable(vid); 
+					} catch (MalformedDomgraphException e) {
+						throw new SAXException(e);
+					}
+				} else if (vid.startsWith("h")) {
+					try {
+						codec.tellHandle(vid); 
+					} catch (MalformedDomgraphException e) {
+						throw new SAXException(e);
 					} 
-				} catch (MalformedDomgraphException e) {
-					throw new SAXException(e);
-				}
+				} 
 				
 				if (parent.equals("mrs")) {
 					this.top = vid;
@@ -95,52 +101,61 @@ public class MrsXmlInputCodec extends InputCodec {
 			} else if (qName.equals("ep")) {
 				attrs = new TreeMap<String,String>();
 			}
-			
+			chars = new StringBuilder();
 			parents.push(qName);
 		}
 		
 		public void endElement (String uri, String name, String qName) throws SAXException
 		{
-			try {
-				if (qName.equals("mrs")) {
+			if (qName.equals("mrs")) {
+				try {
 					codec.setTopHandleAndFinish(top);	
-				} else if (qName.equals("hcons")) {
-					codec.addDomEdge(hi, lo);  			
-				} else if (qName.equals("ep")) {
-					codec.addRelation(handle, label, attrs);			
-				} else if (qName.equals("fvpair")) {
-					attrs.put(attr, value);
+				} catch (MalformedDomgraphException e) {
+					throw new SAXException(e);
 				}
-			} catch (MalformedDomgraphException e) {
-				throw new SAXException(e);
+			} else if (qName.equals("ep")) {
+				try {
+					codec.addRelation(handle, label, attrs);
+				} catch (MalformedDomgraphException e) {
+					throw new SAXException(e);
+				}
+			} else if (qName.equals("hcons")) {
+				codec.addDomEdge(hi, lo);  			
+			} else if (qName.equals("fvpair")) {
+				attrs.put(attr, value);
+			} else if (qName.equals("pred")) {
+				label = chars.toString();
+			} else if (qName.equals("rargname")) {
+				attr = chars.toString();
+			} else if (qName.equals("constant")) {
+				value = chars.toString();
 			}
+			
 			parents.pop();
 		}
 		
 		public void characters (char ch[], int start, int length)
-		{
-			String parent = parents.peek();
-			
-			if (parent.equals("pred")) {
-				this.label = new String(ch, start, length);
-			} else if (parent.equals("rargname")) {
-				this.attr = new String(ch, start, length);
-			} else if (parent.equals("constant")) {
-				this.value = new String(ch, start, length);
-			}
+		{		
+			chars.append(ch, start, length);
 		}
 	}
 	
-	public void decode(Reader inputStream, DomGraph graph, NodeLabels labels) throws IOException, ParserException, MalformedDomgraphException
+	public void decode(Reader inputStream, DomGraph graph, NodeLabels labels) throws MalformedDomgraphException, IOException, ParserException
 	{
 		codec = new MrsCodec(graph, labels);
 		
 		try {
 			SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
-			parser.parse(new InputSource(inputStream), new XmlParser());
-		}  catch(IOException e) {
+			parser.parse(new InputSource(inputStream), new XmlParser());		
+		} catch(IOException e) {
 			throw e;
-		} catch(Throwable e) {
+		} catch(SAXException e) {
+			if( (e.getException() != null) && (e.getException() instanceof MalformedDomgraphException) ) {
+				throw (MalformedDomgraphException) e.getException();
+			} else {			
+				throw new ParserException(e);
+			}
+		} catch(ParserConfigurationException e) {
 			throw new ParserException(e);
 		}
 	}
