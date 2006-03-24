d9e0fff55efabada30026484c9599805079da716
regneri <regneri@localhost>
2006-03-24 11:05:45 +0000

    some documentation

--- src/de/saar/chorus/domgraph/codec/holesem/HolesemComsemOutputCodec.java
+++ src/de/saar/chorus/domgraph/codec/holesem/HolesemComsemOutputCodec.java
@@ -35,37 +35,56 @@ import de.saar.chorus.domgraph.graph.EdgeType;
  */
 
 public class HolesemComsemOutputCodec extends GraphOutputCodec {
-    public static String getName() {
+    
+	/**
+	 * 
+	 * @return
+	 */
+	public static String getName() {
         return "holesem-comsem";
     }
     
+    /**
+     * 
+     * @return
+     */
     public static String getExtension() {
         return ".hs.pl";
     }
 
     
-    
+  
     public void encode_graph(DomGraph graph, NodeLabels labels, Writer writer)
             throws IOException, MalformedDomgraphException {
         
-        if( !graph.isNormal() || !graph.isHypernormallyConnected() ) {
+    	// check whether the graph is applicable for holesemantics conversion
+        if( !graph.isNormal() || !graph.isHypernormallyConnected() ||
+        		! encodeGraphApplicable(graph, labels)) {
             throw new MalformedDomgraphException();
         }
         
-        int closeBrackets = 0;
+        int closeBrackets = 0;	
         StringBuffer content = new StringBuffer();
         String top = null;
         boolean naked_top_hole = false;
         Set<String> labelledLeaves = new HashSet<String>();
         List<String> conjunction = new ArrayList<String>();
         List<String> hs_labels = new ArrayList<String>();
-        Map<String,String> var_nodes = new HashMap<String,String>();
+       
+        // artificial top-fragment (for the case that it's needed) 
         String topname = "_utool_top_";
         String supertopname = "_utool_top_1_";
         
                 
+        /*
+         * TODO check for cycles here
+         * Is there a top-node dominating all other nodes (and
+         * thus having no incoming edges)?
+         */
         if( getTopNodes(graph).size() !=  1 ) {
-        	System.out.println("Top-Fragment");
+        	// if there are no cycles, there can't be more
+        	// than one root of that kind.
+        	// so, if there is none, we create one.
         	content.append("some(" + supertopname + ",and(label(" +
         			supertopname + "),");
         	content.append("some(" + topname + ",and(hole(" +
@@ -74,16 +93,19 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
         	
         	closeBrackets += 4;
         } else {
-        
+        	// if there is a root, we don't need to create one.
         	top = getTopNodes(graph).get(0);
         	topname = varifyIfNecessary(top);
         }
         
+        // node conversion
         for( String node : graph.getAllNodes() ) {
         	NodeData data = graph.getData(node);
         	String nodeName = varifyIfNecessary(node);
         	
+        	// labelled nodes
         	if( data.getType() == NodeType.UNLABELLED ) {
+        		// unlabelled nodes are holes
         		content.append("some(" +  nodeName +
         				",and(hole(" + nodeName + "),");
         		
@@ -92,8 +114,10 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
         			naked_top_hole = true;
         		}
         	} else if(graph.getOutEdges(node, null).size() == 0) {
-    			labelledLeaves.add(node);
+    			// storage for labelled leaves
+        		labelledLeaves.add(node);
     		} else {
+    			// labelled nodes
     			content.append("some(" +  nodeName +
         				",and(label(" + nodeName + "),");
     			
@@ -102,6 +126,7 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
     		}
         }
         
+        // processing labelled nodes that are no leaves
         for( String node : hs_labels ) {
         	NodeData data = graph.getData(node);
         	String nodeName = varifyIfNecessary(node);
@@ -109,15 +134,17 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
         	
         	List<String> children = graph.getChildren(node,EdgeType.TREE);
         	
-        	
+        	// if there was no root originally and we
+        	// created an artificial one, we draw an edge
+        	// between this top fragment and all labelled nodes.
         	if( (top == null) || naked_top_hole ) {
         		conjunction.add("leq(" + nodeName + "," + topname + ")");
         	}
-        	        	
+        	
+        	// resolving the predicate denoted by the node
         	StringBuffer nextConjunct = new StringBuffer();
         	
         	if( isLogicalLabel(label)) {
-        		
         		nextConjunct.append(atomifyIfNecessary(label));
         		
         		nextConjunct.append("(" + nodeName);
@@ -128,6 +155,7 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
         		
         	}
         	
+        	// adding (tree-)edges
         	for( String child : children ) {
         		NodeData holeData = graph.getData(child);
         		nextConjunct.append(",");
@@ -141,6 +169,8 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
         	nextConjunct.append(")");
         	conjunction.add(nextConjunct.toString());
         	
+        	// adding domincance edges (out of the node's
+        	// children)
         	for(String child : children) {
         		for( String domChild : graph.getChildren(child, 
         				EdgeType.DOMINANCE)) {
@@ -156,6 +186,8 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
         
         int i = 0, len = conjunction.size();
         
+        // add the conjunction consisting of 
+        // the conjunctions stored
         for( String conjunct : conjunction ) {
         	if( i < len -1 ) {
         		content.append("and(" + conjunct + ",");
@@ -166,17 +198,26 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
         	i++;
         }
         
+        // closing the brackets
         for( int h = 0; h < closeBrackets; h++ ) {
         	content.append(")");
         }
-       content.append("\n");
+        
+        
+        content.append("\n");
+        
+        // let the writer write everything stored
         writer.write(content.toString());
         writer.flush();
-   //     throw new UnsupportedOperationException();
-        // TODO Auto-generated method stub
-
     }
     
+    /**
+     * Resolving the roots of the graph, i.e.
+     * the fragment roots with no incoming domincance edges.
+     * 
+     * @param graph the graph
+     * @return a List of roots
+     */
     private List<String> getTopNodes(DomGraph graph) {
     	List<String> roots = new ArrayList<String>();
     	
@@ -188,36 +229,54 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
     	return roots;
     }
 
-    @Override
+    /**
+     * 
+     */
     public void print_header(Writer writer) throws IOException {
-    	 //writer.write("%%  autogenerated by Domgraph/Java (see utool.sourceforge.net for details)\n");
+    	 writer.write("%%  autogenerated by Domgraph/Java (see utool.sourceforge.net for details)\n");
     	
     }
 
-    @Override
+    /**
+     * 
+     */
     public void print_footer(Writer writer) throws IOException {
     	 writer.flush();
 
     }
 
-    @Override
+    /**
+     * 
+     */
     public void print_start_list(Writer writer) throws IOException {
     	  writer.write( "[\n");
 
     }
 
-    @Override
+    /**
+     * 
+     */
     public void print_end_list(Writer writer) throws IOException {
     	  writer.write("]\n");
 
     }
 
-    @Override
+    /**
+     * 
+     */
     public void print_list_separator(Writer writer) throws IOException {
         writer.write(System.getProperty("line.separator"));
 
     }
     
+    /**
+     * Checks whether a String has to be preceeded by "_"
+     * to form a valid PROLOG variable name; if so, it returns
+     * the modified string, otherwise the string itself.
+     * 
+     * @param var the original string
+     * @return the string as valid prolog variable
+     */
     private String varifyIfNecessary(String var) {
     	String ret = "";
     	if( (var != null) && var.length() > 0 ) {
@@ -231,6 +290,16 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
     	return ret;
     }
     
+    /**
+     * Checks whether a String has to be enclosed by "\'"
+     * to form a valid PROLOG atom; if so, it returns
+     * the modified string, otherwise the string itself.
+     *  
+     *  TODO correct the test for valid ASCII characters
+     *  
+     * @param var the original String
+     * @return the String as valid prolog atom
+     */
     private String atomifyIfNecessary(String var) {
     	
     	String ret = var;
@@ -247,19 +316,37 @@ public class HolesemComsemOutputCodec extends GraphOutputCodec {
     			}
     			if(! ((Character.isLetterOrDigit(chars[i]) && 
     					( (-1 < (int) chars[i]) && ((int) chars[i] < 128) ))) ) {
-    								return "\'" + var + "\'";
+    				// TODO correct this! obviously this does not accept "_",
+    				// at least not each time it should do so.
+    				return "\'" + var + "\'";
     			}
     		}
     	}
     	return ret;	
     }
     
+    /**
+     * Checks whether a String represents a logical
+     * label.
+     * @param exp the expression
+     * @return true if it is a logical label.
+     */
     private boolean isLogicalLabel(String exp) {
     	return ("some".equals(exp) || "all".equals(exp)
     			|| "and".equals(exp) || "or".equals(exp) 
     			|| "imp".equals(exp) || "not".equals(exp));
     }
     
+    /**
+     * Checks whether a graph is applicable for holesem-comsem
+     * conversion.
+     * 
+     * TODO revise me!!!
+     * 
+     * @param graph the graph
+     * @param labels the node labels 
+     * @return true if the graph can be converted
+     */
     private boolean encodeGraphApplicable(DomGraph graph, 
     		NodeLabels labels) {
     	
