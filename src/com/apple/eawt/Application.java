d816b174a5e4ec67d1de55c30d74331fab3641bf
koller <koller@localhost>
2007-08-24 03:22:58 +0000

    Better support for the MacOS application menu (About/Preferences/Quit). This fixes #248.

---
+++ src/com/apple/eawt/Application.java
@@ -0,0 +1,31 @@
+package com.apple.eawt;
+
+public class Application {
+	public static Application getApplication() {
+		return null;
+	}
+	
+	public void addApplicationListener(ApplicationListener x) {
+		
+	}
+	
+	public void addAboutMenuItem() {
+		
+	}
+	
+	public void addPreferencesMenuItem() {
+		
+	}
+	
+	public void setEnabledAboutMenu(boolean enable) {
+		
+	}
+	
+	public void setEnabledPreferencesMenu(boolean enable) {
+		
+	}
+	
+	public void removeAboutMenuItem() {
+		
+	}
+}
---
+++ src/de/saar/chorus/ubench/MacIntegration.java
@@ -0,0 +1,76 @@
+package de.saar.chorus.ubench;
+
+import javax.swing.JOptionPane;
+
+import com.apple.eawt.Application;
+import com.apple.eawt.ApplicationEvent;
+import com.apple.eawt.ApplicationListener;
+
+import de.saar.chorus.domgraph.GlobalDomgraphProperties;
+import de.saar.chorus.domgraph.graph.DomGraph;
+import de.saar.chorus.domgraph.graph.NodeLabels;
+
+public class MacIntegration {
+	public static boolean isMac() {
+		return System.getProperty("mrj.version") != null;
+	}
+	
+	public static void integrate() {
+		if( isMac() ) {
+			System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Utool");
+			System.setProperty("apple.laf.useScreenMenuBar", "true");
+			
+			Application app = Application.getApplication();
+
+			app.addAboutMenuItem();
+			app.setEnabledAboutMenu(true);
+			
+			app.addPreferencesMenuItem();
+			app.setEnabledPreferencesMenu(true);
+			
+			app.addApplicationListener(new UtoolApplicationListener());
+			
+		}
+	}
+	
+	private static class UtoolApplicationListener implements ApplicationListener {
+		@Override
+		public void handleAbout(ApplicationEvent e) {
+			Ubench.getInstance().displayAboutDialog();
+			e.setHandled(true);
+		}
+
+		@Override
+		public void handlePreferences(ApplicationEvent e) {
+			Ubench.getInstance().setPreferenceDialogVisible(true);
+			e.setHandled(true);
+		}
+		
+		@Override
+		public void handleQuit(ApplicationEvent e) {
+			Ubench.getInstance().quit();
+			e.setHandled(true);
+		}
+
+		public void handleOpenApplication(ApplicationEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void handlePrintFile(ApplicationEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void handleReOpenApplication(ApplicationEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void handleOpenFile(ApplicationEvent event) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+}
