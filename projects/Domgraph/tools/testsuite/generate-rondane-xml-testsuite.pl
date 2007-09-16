5fc69d9fa078ad7a685af31fa84df5b4f4061378
koller <koller@coli.uni-saarland.de>
2007-09-16 21:49:02 +0000

    - Implemented a new infrastructure for running corpus testsuites. The testsuite is extracted from the corpus using the generate-rondane-xml-testsuite.pl script and stored in an XML file. The testing framework then iterates over the entries in the XML file (yay Groovy!) and ensures that Utool performs on this corpus exactly like it did when the testsuite was extracted.
    - Deleted the horrible old attempt at doing the same by having a 100.000-line Java
    program that spelled out all the dominance graphs.
    
    - Fixed an extremely embarrassing bug in the domcon-oz output codec that was exposed
    by the testsuite.

---
+++ projects/Domgraph/tools/testsuite/generate-rondane-xml-testsuite.pl
@@ -0,0 +1,157 @@
+# This script generates an XML testsuite from a collection
+# of USRs.  This testsuite can then be used e.g. in the context
+# of the RondaneTestsuite to make sure that Utool still behaves
+# like it did when this script was run.
+#
+# To generate the XML file, change to a directory that has
+# USRs in individual files, and run "perl generate-testsuite.pl <tsname>".
+# This will generate a file "tsname.xml" in the current directory
+# containing one testcase for each USR file.  When generating an
+# MRS-based testsuite, make sure that the directory also contains
+# non-nets.
+#
+# Currently the script expects files with extension .mrs.pl
+# encoded in mrs-prolog, but this can be changed easily.
+
+use IO::Socket;
+
+sub xmlify {
+  my $usr = shift;
+
+    # encode entities
+    $usr =~ s[%.*][]g;
+    $usr =~ s[\n][ ]g;
+    $usr =~ s[&][&amp;]sg;
+    $usr =~ s["][&quot;]sg;  #"
+    $usr =~ s['][&apos;]sg;  #' 
+    $usr =~ s[<][&lt;]sg;
+    $usr =~ s[>][&gt;]sg;
+
+  return $usr;
+}
+  
+
+# return 1 iff conversion was successful
+sub convert {
+    my $id = shift;
+    my $codec = shift;
+    my $usr = shift;
+
+    my $socket = IO::Socket::INET->new("localhost:2802")
+    or die $!;
+    
+    print $socket "<utool cmd='convert' output-codec='domcon-oz'>\n";
+    print $socket "  <usr codec='$codec' string='$usr' />\n";
+    print $socket "</utool>\n";
+    $socket->shutdown(1);
+
+    my $answer = join(' ', <$socket>);
+    close $socket;
+    
+    if( $answer =~ /<error/ ) {
+      $answer =~ /code=.([^"']*)/;
+      print OUT "    <domgraph error='$1' />\n";
+      return 0;
+    } else {
+      $answer =~ /usr=.([^"']*)/;
+      print OUT "    <domgraph string='$1' />\n";
+      return 1;
+    }
+  }
+
+
+sub classify {
+    my $id = shift;
+    my $codec = shift;
+    my $usr = shift;
+
+    my $socket = IO::Socket::INET->new("localhost:2802")
+    or die $!;
+    
+    print $socket "<utool cmd='classify'>\n";
+    print $socket "  <usr codec='$codec' string='$usr' />\n";
+    print $socket "</utool>\n";
+    $socket->shutdown(1);
+
+    my $answer = join(' ', <$socket>);
+    close $socket;
+  
+    if( $answer =~ /code=.([^"']*)/ ) {
+      print OUT "    <classify code='$1' />\n";
+    }
+}
+
+
+sub solve {
+    my $id = shift;
+    my $codec = shift;
+    my $usr = shift;
+
+    my $socket = IO::Socket::INET->new("localhost:2802")
+    or die $!;
+    
+    print $socket "<utool cmd='solvable'>\n";
+    print $socket "  <usr codec='$codec' string='$usr' />\n";
+    print $socket "</utool>\n";
+    $socket->shutdown(1);
+
+    my $answer = join(' ', <$socket>);
+    close $socket;
+
+    if( $answer =~ /solvable=.true/ ) {
+      ($count) = ($answer =~ /count=.([^"']*)/);
+      ($chartsize) = ($answer =~ /chartsize=.([^"']*)/);
+      print OUT "    <solve solvable='true' count='$count' chartsize='$chartsize' />\n";
+    } else {
+      print OUT "    <solve solvable='false' />\n";
+    }
+  }
+
+
+
+
+
+################# MAIN ######################
+
+$testsuitename = $ARGV[0];
+$codec = 'mrs-prolog';
+$time = localtime();
+
+opendir(DIR, ".") || die "$!";
+@mrsfiles = grep { /\.mrs\.pl$/ } readdir(DIR);
+closedir DIR;
+
+open OUT, ">$testsuitename.xml" or die "$!";
+
+print OUT <<"END";
+<?xml version='1.0' ?>
+
+<testsuite name='$testsuitename' generated-at='$time'>
+END
+
+
+for $filename (@mrsfiles) {
+    print STDERR "Processing $filename ... \n";
+
+    ($sentid) = ($filename =~ /^(\d+)/);
+
+    open MRSFILE, $filename or die "$!";
+    $usr = xmlify(join('', <MRSFILE>));
+    close MRSFILE;
+
+    print OUT "  <usr id='$sentid' codec='$codec' string='$usr'>\n";
+
+    if( convert($sentid, $codec, $usr) ) {
+      classify($sentid, $codec, $usr);
+      solve($sentid, $codec, $usr);
+    }
+
+    print OUT "  </usr>\n";
+}
+
+print OUT <<'EOP';
+</testsuite>
+EOP
+
+
+    
--- test/de/saar/chorus/domgraph/codec/domcon/DomconOzOutputCodecTest.groovy
+++ test/de/saar/chorus/domgraph/codec/domcon/DomconOzOutputCodecTest.groovy
@@ -5,6 +5,7 @@ import de.saar.chorus.domgraph.codec.*;
 import de.saar.chorus.domgraph.codec.domcon.*;
 import de.saar.chorus.domgraph.codec.basic.*;
 import de.saar.chorus.domgraph.codec.holesem.*;
+import de.saar.chorus.domgraph.codec.mrs.*;
 
 import java.util.*;
 import java.io.*;
@@ -45,6 +46,14 @@ class DomconOzOutputCodecTest extends GroovyTestCase {
 	public void testReversibilityHolesem14() {
 		InputCodec holesemcodec = new HolesemComsemInputCodec();
 		holesemcodec.decode(new StringReader("some(_A,and(hole(_A),some(_B,and(label(_B),some(_C,some(_D,some(_E,some(_F,some(_G,and(hole(_C),and(label(_D),and(label(_E),and(label(_F),and(all(_E,_G,_F),and(imp(_F,_D,_C),and(leq(_B,_C),and(leq(_E,_A),and(some(_H,some(_I,some(_J,and(hole(_H),and(label(_I),and(label(_J),and(and(_D,_I,_H),and(leq(_D,_A),and(leq(_J,_H),and(some(_K,some(_L,some(_M,some(_N,some(_O,and(hole(_K),and(label(_L),and(label(_M),and(label(_N),and(some(_M,_O,_N),and(and(_N,_L,_K),and(leq(_J,_K),and(leq(_M,_A),and(and(pred1(_L,restaurant,_O),leq(_L,_A)),and(pred2(_J,in,_O,_G),leq(_J,_A)))))))))))))))),and(pred1(_I,man,_G),leq(_I,_A)))))))))))),some(_P,some(_Q,some(_R,some(_S,some(_T,and(hole(_P),and(label(_Q),and(label(_R),and(label(_S),and(some(_R,_T,_S),and(and(_S,_Q,_P),and(leq(_B,_P),and(leq(_R,_A),and(some(_U,some(_V,some(_W,and(hole(_U),and(label(_V),and(label(_W),and(and(_Q,_V,_U),and(leq(_Q,_A),and(leq(_W,_U),and(some(_X,some(_Y,some(_Z,some(_A1,some(_B1,and(hole(_X),and(label(_Y),and(label(_Z),and(label(_A1),and(some(_Z,_B1,_A1),and(and(_A1,_Y,_X),and(leq(_W,_X),and(leq(_Z,_A),and(and(pred1(_Y,car,_B1),leq(_Y,_A)),and(pred2(_W,with,_B1,_T),leq(_W,_A)))))))))))))))),and(pred1(_V,woman,_T),leq(_V,_A)))))))))))),and(pred2(_B,know,_T,_G),leq(_B,_A))))))))))))))))))))))))))))))))))"), graph, labels);
+		assertReversibility(graph, labels);
+	}
+	
+	// a rather embarrassing former bug, in which only the first symbol of each
+	// label would be checked for characters that had to be quote-escaped
+	public void testReversibilityRondane1() {
+		InputCodec mrscodec = new MrsPrologInputCodec(Normalisation.nets, LabelStyle.plain);
+		mrscodec.decode(new StringReader("psoa(h1,e2,[rel('prpstn_m',h1,[attrval('ARG0',e2),attrval('MARG',h3),attrval('PSV',u4),attrval('TPC',u5)]),rel('_the_q',h6,[attrval('ARG0',x8),attrval('RSTR',h9),attrval('BODY',h7)]),rel('_well+known_a_1',h10,[attrval('ARG0',e11),attrval('ARG1',x8)]),rel('_and_c',h12,[attrval('ARG0',e15),attrval('L-HNDL',h10),attrval('L-INDEX',e11),attrval('R-HNDL',h14),attrval('R-INDEX',e13)]),rel('_historic_a_1',h14,[attrval('ARG0',e13),attrval('ARG1',x8)]),rel('compound',h12,[attrval('ARG0',e17),attrval('ARG1',x8),attrval('ARG2',x16)]),rel('proper_q',h18,[attrval('ARG0',x16),attrval('RSTR',h19),attrval('BODY',h20)]),rel('named',h21,[attrval('ARG0',x16),attrval('CARG','aurlandsdalen')]),rel('_valley_n_of',h12,[attrval('ARG0',x8),attrval('ARG1',i22)]),rel('_be_v_id',h23,[attrval('ARG0',e2),attrval('ARG1',x8),attrval('ARG2',x24)]),rel('_once_a_1',h23,[attrval('ARG0',e25),attrval('ARG1',e2)]),rel('part_of',h26,[attrval('ARG0',x24),attrval('ARG1',x27)]),rel('udef_q',h28,[attrval('ARG0',x24),attrval('RSTR',h29),attrval('BODY',h30)]),rel('card',h26,[attrval('ARG0',i31),attrval('ARG1',x24),attrval('CARG','1')]),rel('_the_q',h32,[attrval('ARG0',x27),attrval('RSTR',h34),attrval('BODY',h33)]),rel('_main_a_1',h35,[attrval('ARG0',e36),attrval('ARG1',x27)]),rel('_route_n_1',h35,[attrval('ARG0',x27)]),rel('_between_p',h35,[attrval('ARG0',e37),attrval('ARG1',x27),attrval('ARG2',x38)]),rel('_the_q',h39,[attrval('ARG0',x38),attrval('RSTR',h41),attrval('BODY',h40)]),rel('_eastern_a_1',h42,[attrval('ARG0',e43),attrval('ARG1',x38)]),rel('_and_c',h44,[attrval('ARG0',e47),attrval('L-HNDL',h42),attrval('L-INDEX',e43),attrval('R-HNDL',h46),attrval('R-INDEX',e45)]),rel('_western_a_1',h46,[attrval('ARG0',e45),attrval('ARG1',x38)]),rel('_part_n_1',h44,[attrval('ARG0',x38)]),rel('_of_p',h44,[attrval('ARG0',e48),attrval('ARG1',x38),attrval('ARG2',x49)]),rel('proper_q',h50,[attrval('ARG0',x49),attrval('RSTR',h51),attrval('BODY',h52)]),rel('named',h53,[attrval('ARG0',x49),attrval('CARG','norway')])],hcons([qeq(h51,h53),qeq(h41,h44),qeq(h34,h35),qeq(h29,h26),qeq(h19,h21),qeq(h9,h12),qeq(h3,h23)]))"), graph, labels);
 		assertReversibility(graph, labels);
 	}
 	
