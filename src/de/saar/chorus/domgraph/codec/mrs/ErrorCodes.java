8e9ff30e3fd5d0bfb024d1d5a92ed6a27184656d
koller <koller@localhost>
2007-09-20 21:16:48 +0000

    Fixed error code that was outside of the legal range.

--- src/de/saar/chorus/domgraph/codec/mrs/ErrorCodes.java
+++ src/de/saar/chorus/domgraph/codec/mrs/ErrorCodes.java
@@ -1,12 +1,13 @@
 package de.saar.chorus.domgraph.codec.mrs;
 
 public class ErrorCodes {
-	 
+	// these error codes can be combined with each other by bitwise OR
     public static final int  NOT_NORMAL = 1;
     public static final int  NOT_WEAKLY_NORMAL = 2;
     public static final int  NOT_LEAF_LABELLED = 4;
     public static final int  NOT_HYPERNORMALLY_CONNECTED = 8;
     
+    // these indicate separate errors and are never OR'ed with anything
     public static final int  NOT_WELLFORMED = 16;
-	public static final int NO_UNIQUE_TOP_FRAGMENT = 32;
+	public static final int NO_UNIQUE_TOP_FRAGMENT = 17;
 }
