2b019a76a3128e69a77f99de8544fc9c4b72359c
regneri <regneri@localhost>
2007-10-18 09:33:53 +0000

    A cookie for someone who can prove me why this works.
    This was actually meant to be _faster_ than the solver.
    Well, now it is, up to 10 times (and more, depending on the solvers current performance). But I don't trust this implementation.
    However, there is a fallback which is save and not too much slower.

--- src/nl/rug/discomm/udr/chart/ModifiableChart.java
+++ src/nl/rug/discomm/udr/chart/ModifiableChart.java
@@ -1,6 +1,7 @@
 package nl.rug.discomm.udr.chart;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -19,41 +20,14 @@ import de.saar.chorus.domgraph.chart.Split;
  */
 public class ModifiableChart extends Chart {
 	
-	private List<Set<String>> orderedSubgraphs;
 	
-	public ModifiableChart() {
-		super();
-		orderedSubgraphs = null;
-	}
-	
-	/**
-	 * This provides the subgraph contained in this chart, ordered from
-	 * toplevel subgraphs followed by the child graphs of their splits, and
-	 * so on.
-	 * For introducing new dominances, one needs this ordered subgraph list
-	 * such that the splits are deleted in the right order. Without this ordering,
-	 * some subgraphs that could actually be deleted are still referenced when
-	 * deleting their last split.
-	 * This method is to make sure that this list will be computed only once.
-	 * 
-	 * @return an ordered list of subgraphs in this chart.
-	 */
-	private List<Set<String>> getAllSubgraphs() {
 
-		if(orderedSubgraphs == null) {
-			orderedSubgraphs = new ArrayList<Set<String>>();
-			for(Set<String> top : getToplevelSubgraphs()) {
-				addOrderedSubgraphs(top);
-			}
-		}
-		return orderedSubgraphs;
-	}
 
 	
-	public void deleteSubgraph(Set<String> subgraph) {
+	/*public void deleteSubgraph(Set<String> subgraph) {
 		super.deleteSubgraph(subgraph);
 		orderedSubgraphs.remove(subgraph);
-	}
+	}*/
 	
 	/**
 	 * This introduces a dominance edge from the given source to the given target node
@@ -69,84 +43,108 @@ public class ModifiableChart extends Chart {
 	 */
 	public void addDominance(String src, String tgt) throws UnsupportedOperationException {
 
-		List<Set<String>> copyOfSubgraphs = new ArrayList<Set<String>>(getAllSubgraphs());
-		
+		long time = System.currentTimeMillis();
 		
 		// check for all subgraphs whether they are concerned with the
 		// new dominance edge.
-		for( Set<String> subgraph : copyOfSubgraphs ) {
+		for( Set<String> subgraph : getToplevelSubgraphs() ) {
 
-			// relevant subgraphs contain the source and
-			// the target of the edge.
-			if( subgraph.contains(src) && 
-					subgraph.contains(tgt) ) {
+			restrictSubgraph(subgraph, src, tgt, new HashSet<Set<String>>());
+		}
+	}
+	
+	
+	private boolean delete(Split split, String src, String tgt) {
+		
+		
+		/*
+		 * A Split has to be deleted iff
+		 * its root fragment is dominated by the new dominance edge 
+		 * (provided that the source node is in the same subgraph) or
+		 * its root fragment is not free anymore after introducing the edge.
+		 */
+		
+		// if the dominance edge is within the subgraph,
+		// a split rooted by the target node is not valid anymore.
+		if(! split.getRootFragment().equals(tgt)) {
+			boolean amStillFree = true;
+			boolean foundOne = false;
+			
 
-				List<Split> splits = 
-					getSplitsFor(subgraph);
-				
-				// for the new list of splits
-				List<Split> modified = new ArrayList<Split>();
-				
-				/*
-				 * A Split has to be deleted iff
-				 * its root fragment is dominated by the new dominance edge 
-				 * (provided that the source node is in the same subgraph) or
-				 * its root fragment is not free anymore after introducing the edge.
-				 */
-				for(Split split : splits) {
-					
-					// if the dominance edge is within the subgraph,
-					// a split rooted by the target node is not valid anymore.
-					if(! split.getRootFragment().equals(tgt)) {
-						boolean amStillFree = true;
-						boolean foundOne = false;
-
-						/*
-						 * checking freeness of the root fragment.
-						 * the (formerly free) root fragment is not free anymore iff
-						 * two nodes of two different subgraphs have been
-						 * connected by the dominance edge.
-						 */
-						for (Set<String> children :
-							split.getAllSubgraphs()) {
-							if(children.contains(src)) {
-								if(foundOne) {
-									amStillFree = false;
-									break;
-								} else {
-									foundOne = true;
-								}
-							} else if (children.contains(tgt)) {
-								if(foundOne) {
-									amStillFree = false;
-									break;
-								} else {
-									foundOne = true;
-								}
-							}
-						}
-
-						if( amStillFree ) {
-							// 'really' free fragment
-							modified.add(split);
-						}
+			/*
+			 * checking freeness of the root fragment.
+			 * the (formerly free) root fragment is not free anymore iff
+			 * two nodes of two different subgraphs have been
+			 * connected by the dominance edge.
+			 */
+			for (Set<String> children :
+				split.getAllSubgraphs()) {
+				if(children.contains(src)) {
+					if(foundOne) {
+						amStillFree = false;
+						break;
+					} else {
+						foundOne = true;
+					}
+				} else if (children.contains(tgt)) {
+					if(foundOne) {
+						amStillFree = false;
+						break;
+					} else {
+						foundOne = true;
 					}
 				}
-
-				setSplitsForSubgraph(subgraph, modified);
-
 			}
+			return  ! amStillFree;
+		} else {
+			return true;
 		}
 	}
+	
 
-	private void addOrderedSubgraphs(Set<String> subgraph) {
-		orderedSubgraphs.add(subgraph);
-		if(containsSplitFor(subgraph)) {
-			for(Split split : getSplitsFor(subgraph)) {
-				for(Set<String> child : split.getAllSubgraphs()) {
-					addOrderedSubgraphs(child);
+	private void restrictSubgraph(Set<String> subgraph, String src, String tgt,
+			Set<Set<String>> visited) {
+
+		long time = System.currentTimeMillis();
+		if(! visited.contains(subgraph)) {
+
+			visited.add(subgraph);
+
+			if( subgraph.contains(src) && subgraph.contains(tgt) ) {
+
+				// for the new list of splits
+				List<Split> modified = new ArrayList<Split>();
+				boolean changed = false;
+
+				if(containsSplitFor(subgraph)) {
+					for(Split split : getSplitsFor(subgraph) ) {
+						if(! delete(split, src, tgt)) {
+							modified.add(split);
+							for(Set<String> child : split.getAllSubgraphs() ) {
+								restrictSubgraph(child, src, tgt,visited);
+							}
+						} else {
+							changed = true;
+						}
+					}
+
+					if(changed) {
+						setSplitsForSubgraph(subgraph, modified);
+					}
+
+				/*	for(Split split : modified ) {
+						for(Set<String> child : split.getAllSubgraphs() ) {
+							restrictSubgraph(child, src, tgt,visited);
+						}
+					}*/
 				}
 			}
+
 		}
+
 	}
+	
+
+
+
  }
