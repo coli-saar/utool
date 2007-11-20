1711e0e0f2eea8b6b9919ff8546a695f0cafaca2
koller <koller@coli.uni-saarland.de>
2007-11-20 22:45:28 +0000

    Implemented the textbook algorithm for the union-find problem. This will be useful all over the place (in particular, for the equality constraints in the RMRS codec).

--- test/DomgraphTestSuite.java
+++ test/DomgraphTestSuite.java
@@ -9,22 +9,22 @@ public class DomgraphTestSuite extends TestSuite {
     private static final String TEST_ROOT = "test/";
     private static final String DOMGRAPH = "de/saar/chorus/domgraph/";
     private static final String SUFFIX = "Test.groovy";
-    
+
     private static TestSuite suite = new TestSuite();
     private static GroovyTestSuite gsuite = new GroovyTestSuite();
-    
+
     private static void addDomgraphTest(String name) throws Exception {
     	suite.addTestSuite(gsuite.compile(TEST_ROOT + DOMGRAPH + name + SUFFIX));
     }
-    
+
     private static void addTest(String name) throws Exception {
     	suite.addTestSuite(gsuite.compile(TEST_ROOT + name + SUFFIX));
     }
-    
+
     public static Test suite() throws Exception {
-        
+
         //suite.addTestSuite(FooTest.class);  // non-groovy test cases welcome, too.
-        
+
     	addDomgraphTest("codec/CodecManager");
     	addDomgraphTest("codec/basic/Chain");
     	addDomgraphTest("codec/domcon/DomconOzInputCodec");
@@ -37,7 +37,8 @@ public class DomgraphTestSuite extends TestSuite {
         addDomgraphTest("layout/chartlayout/DomGraphChartLayout");
         addDomgraphTest("layout/domgraphlayout/DomGraphLayout");
         addDomgraphTest("equivalence/RedundancyElimination");
-    	
+        addTest("de/saar/basic/UnionFind");
+
         return suite;
     }
 }
