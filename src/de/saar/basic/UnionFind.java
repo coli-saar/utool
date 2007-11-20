f7b7cd00cd775ef2cb0a2ba7513d2dee72daf1e9
koller <koller@coli.uni-saarland.de>
2007-11-20 22:49:19 +0000

    Wrote some documentation.

--- src/de/saar/basic/UnionFind.java
+++ src/de/saar/basic/UnionFind.java
@@ -4,9 +4,18 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * An implementation of the textbook algorithm for the union-find problem. This
+ * implementation (based on the union-by-size and path compression heuristics)
+ * computes a sequence of union and find operations in nearly linear amortized runtime.
+ *
+ * @author Alexander Koller
+ *
+ * @param <E> the datatype of the elements
+ */
 public class UnionFind<E> {
-    private final Map<E,E> tree;
-    private final Map<E,Integer> treesizes;
+    private final Map<E,E> tree;             // maps each node to its parent (and roots to themselves)
+    private final Map<E,Integer> treesizes;  // maps each root to the number of nodes in its tree
 
     public UnionFind(Collection<E> baseSet) {
         tree = new HashMap<E,E>();
@@ -18,6 +27,12 @@ public class UnionFind<E> {
         }
     }
 
+    /**
+     * Merges the equivalence classes of two elements, x and y.
+     *
+     * @param x
+     * @param y
+     */
     public void union(E x, E y) {
         E xroot = find(x);
         E yroot = find(y);
@@ -42,6 +57,12 @@ public class UnionFind<E> {
         }
     }
 
+    /**
+     * Returns the representative element of the equivalence class of element x.
+     *
+     * @param x
+     * @return
+     */
     public E find(E x) {
         if( tree.get(x).equals(x) ) {
             return x;
