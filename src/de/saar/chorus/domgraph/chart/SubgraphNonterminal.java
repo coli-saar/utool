529ceeed74e0b853f16b2e51997602748183318c
koller <koller@localhost>
2008-01-08 18:16:36 +0000

    Faster hashCode.

--- src/de/saar/chorus/domgraph/chart/SubgraphNonterminal.java
+++ src/de/saar/chorus/domgraph/chart/SubgraphNonterminal.java
@@ -6,21 +6,69 @@ import java.util.Set;
 
 public class SubgraphNonterminal extends HashSet<String> implements GraphBasedNonterminal {
     private String rootForThisFragset;
+    private boolean changed;
+    private int previousHashcode;
+
 
     public SubgraphNonterminal(Collection<String> init) {
         super(init);
+        changed = true;
     }
 
     public SubgraphNonterminal() {
         super();
+        changed = true;
     }
 
-    
+
+
+
+
+    @Override
+    public boolean add(String e) {
+        // TODO Auto-generated method stub
+        changed = true;
+        return super.add(e);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        // TODO Auto-generated method stub
+        changed = true;
+        return super.remove(o);
+    }
+
+
+
+    @Override
+    public boolean equals(Object arg0) {
+        // TODO Auto-generated method stub
+        return super.equals(arg0);
+    }
+
+    @Override
+    public int hashCode() {
+        if( !changed ) {
+            return previousHashcode;
+        } else {
+            previousHashcode = super.hashCode();
+            changed = false;
+            return previousHashcode;
+        }
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> arg0) {
+        changed = true;
+        // TODO Auto-generated method stub
+        return super.removeAll(arg0);
+    }
+
     public String getRootIfSingleton() {
         return rootForThisFragset;
     }
 
-   
+
     public boolean isSingleton(Set<String> roots) {
         int numRoots = 0;
 
@@ -42,19 +90,19 @@ public class SubgraphNonterminal extends HashSet<String> implements GraphBasedNo
 
     private static final long serialVersionUID = 1533989291501267385L;
 
-    
+
     public Set<String> getNodes() {
         return this;
     }
 
-   
+
     public String toString(Set<String> roots) {
         Set<String> s = new HashSet<String>(this);
         s.retainAll(roots);
         return s.toString();
     }
 
- 
+
     public void addNode(String node) {
         add(node);
     }
