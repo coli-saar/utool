7f6060889b01ce3761635711eb9130335764b71e
Alexander Koller <koller@coli.uni-saarland.de>
2010-05-07 23:31:45 +0200

    Experimenting with faster determinization.

--- src/main/java/de/saar/chorus/domgraph/chart/lethal/RelativeNormalFormsComputer.java
+++ src/main/java/de/saar/chorus/domgraph/chart/lethal/RelativeNormalFormsComputer.java
@@ -21,6 +21,7 @@ import static de.uni_muenster.cs.sev.lethal.treeautomata.generic.GenFTAOps.*;
 public class RelativeNormalFormsComputer {
     private static final boolean DEBUG=true;
     private RewriteSystemToTransducer rstt;
+    private static final Stopwatch stopwatch = new Stopwatch();
 
     public RelativeNormalFormsComputer(RewriteSystem weakening, RewriteSystem equivalence, Annotator annotator) {
         rstt = new RewriteSystemToTransducer(weakening, equivalence, annotator);
@@ -28,16 +29,36 @@ public class RelativeNormalFormsComputer {
 
     public FTA reduce(Chart chart, DomGraph graph, NodeLabels labels) {
         if(DEBUG) System.err.println("Converting chart to FTA ...");
+        stopwatch.start("fta");
         EasyFTA chartFta = ChartToFTA.convert(chart, graph, labels);
+        stopwatch.report("fta", "Converted");
 
         if(DEBUG) System.err.println("Computing ctt ...");
+        stopwatch.start("ctt");
         ContextTreeTransducer<RankedSymbol,RankedSymbol,State> ctt = rstt.convert(graph, labels);
+        stopwatch.report("ctt", "CTT computed");
 
         if(DEBUG) System.err.println("Computing pre-image ...");
+        stopwatch.start("pre");
         FTA preImage = ctt.computePreImage(chartFta);
+        stopwatch.report("pre", "Computed");
+
+        /*
+        FTA pre1 = reduceTopDown(preImage);
+        System.err.println("reduced pre-image fta has " + pre1.getRules().size() + " rules");
+
+        FTA reduced = determinizeBU(pre1);
+         * 
+         */
 
         if(DEBUG) System.err.println("Computing complement and intersection ...");
-        FTA reduced = intersectionTD(chartFta, complement(preImage));
+        stopwatch.start("complement");
+        FTA a = complement(preImage);
+        stopwatch.report("complement", "Computed complement");
+
+        stopwatch.start("intersect");
+        FTA reduced = intersectionTD(chartFta, a);
+        stopwatch.report("intersect", "Computed intersection");
 
         return reduced;
     }
--- src/main/java/de/saar/chorus/domgraph/chart/lethal/UtoolReducer.java
+++ src/main/java/de/saar/chorus/domgraph/chart/lethal/UtoolReducer.java
@@ -2,60 +2,81 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.saar.chorus.domgraph.chart.lethal;
 
 import de.saar.chorus.domgraph.chart.Chart;
 import de.saar.chorus.domgraph.chart.ChartSolver;
+import de.saar.chorus.domgraph.chart.SolverNotApplicableException;
 import de.saar.chorus.domgraph.codec.CodecManager;
+import de.saar.chorus.domgraph.codec.CodecRegistrationException;
 import de.saar.chorus.domgraph.codec.InputCodec;
+import de.saar.chorus.domgraph.codec.MalformedDomgraphException;
+import de.saar.chorus.domgraph.codec.ParserException;
 import de.saar.chorus.domgraph.graph.DomGraph;
+import de.saar.chorus.domgraph.graph.DomGraph.PreprocessingException;
 import de.saar.chorus.domgraph.graph.NodeLabels;
 import de.uni_muenster.cs.sev.lethal.languages.RegularTreeLanguage;
 import de.uni_muenster.cs.sev.lethal.symbol.common.RankedSymbol;
 import de.uni_muenster.cs.sev.lethal.tree.common.Tree;
 import de.uni_muenster.cs.sev.lethal.treeautomata.common.FTA;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
  * @author koller
  */
 public class UtoolReducer {
-    public static void main(String[] args) throws Exception {
-        CodecManager codecman = new CodecManager();
-        codecman.registerAllDeclaredCodecs();
 
+    public static void main(String[] args) throws Exception {
         // read USR and convert it to domgraph
-        InputCodec codec = codecman.getInputCodecForFilename(args[0], "");
         DomGraph graph = new DomGraph();
         NodeLabels labels = new NodeLabels();
-        codec.decode(new FileReader(new File(args[0])), graph, labels);
+        load(args[0], graph, labels);
 
         // solve USR
-        Chart chart = new Chart(labels);
-        DomGraph preprocessed = graph.preprocess();
-	boolean solvable = ChartSolver.solve(preprocessed, chart);
-        if( !solvable ) {
-            System.err.println("USR is not solvable, aborting.");
-        } else {
-            System.err.println("USR is solvable, " + chart.countSolvedForms() + " readings.");
-        }
+        Chart chart = solve(labels, graph);
 
         // obtain rewrite systems
-        RewritingSystemParser parser = new RewritingSystemParser();
         RewriteSystem weakening = new RewriteSystem();
         RewriteSystem equivalence = new RewriteSystem();
         Annotator annotator = new Annotator();
-        parser.read(new FileReader(new File(args[1])), weakening, equivalence, annotator);
+        loadRewriteSystem( args[1], weakening, equivalence, annotator);
 
         // convert USR to FTA and reduce it
         RelativeNormalFormsComputer rnfc = new RelativeNormalFormsComputer(weakening, equivalence, annotator);
         FTA fta = rnfc.reduce(chart, graph, labels);
         RegularTreeLanguage<RankedSymbol> rtl = new RegularTreeLanguage(fta);
-        for(Tree t : rtl) {
+        for (Tree t : rtl) {
             System.out.println(t);
         }
+    }
+
+    public static void loadRewriteSystem(String filename, RewriteSystem weakening, RewriteSystem equivalence, Annotator annotator) throws Exception {
+        RewritingSystemParser parser = new RewritingSystemParser();
+        parser.read(new FileReader(new File(filename)), weakening, equivalence, annotator);
+    }
+
+    public static Chart solve(NodeLabels labels, DomGraph graph) throws PreprocessingException, SolverNotApplicableException {
+        Chart chart = new Chart(labels);
+        DomGraph preprocessed = graph.preprocess();
+        boolean solvable = ChartSolver.solve(preprocessed, chart);
+        if (!solvable) {
+            System.err.println("USR is not solvable, aborting.");
+        } else {
+            System.err.println("USR is solvable, " + chart.countSolvedForms() + " readings.");
+        }
+        return chart;
+    }
+
+    public static void load(String filename, DomGraph graph, NodeLabels labels) throws CodecRegistrationException, FileNotFoundException, IOException, ParserException, MalformedDomgraphException {
+        CodecManager codecman = new CodecManager();
+        codecman.registerAllDeclaredCodecs();
+
+        // read USR and convert it to domgraph
+        InputCodec codec = codecman.getInputCodecForFilename(filename, "");
+        codec.decode(new FileReader(new File(filename)), graph, labels);
     }
 }
