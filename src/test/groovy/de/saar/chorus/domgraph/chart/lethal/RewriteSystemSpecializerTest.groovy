03fb8aad10fe5f03a1ad98ddcd657e576b0da485
Alexander Koller <koller@mmci.uni-saarland.de>
2010-07-23 16:43:47 +0200

    Specializer now avoids using the same node name for the same label name in the same specialized rule. (Antoine really wanted this.)

--- src/test/groovy/de/saar/chorus/domgraph/chart/lethal/RewriteSystemSpecializerTest.groovy
+++ src/test/groovy/de/saar/chorus/domgraph/chart/lethal/RewriteSystemSpecializerTest.groovy
@@ -39,6 +39,29 @@ class RewriteSystemSpecializerTest {
     }
 
     @Test
+    public void testSpecializingNoDuplicates() throws Exception {
+        RewritingSystemParser parser = new RewritingSystemParser();
+        RewriteSystem weakening = new RewriteSystem(true);
+        RewriteSystem equivalence = new RewriteSystem(false);
+        Annotator ann = new Annotator();
+
+        parser.read(new StringReader(testRewritingDummy), weakening, equivalence, ann);
+
+
+        DomGraph graph = new DomGraph();
+        NodeLabels labels = new NodeLabels();
+        TestingTools.decodeDomcon(domgraphWithDuplicates, graph, labels);
+
+        RewriteSystemSpecializer specializer = new RewriteSystemSpecializer(graph, labels);
+
+        RewriteSystem sWeakening = specializer.specialize(weakening);
+        assert sWeakening.getAllRules().contains(parseWeakeningRule("[+] every_y(X, every_x(Y,Z)) -> every_x(Y, every_y(X,Z))")) : sWeakening.getAllRules();
+        assert sWeakening.getAllRules().contains(parseWeakeningRule("[+] every_x(X, every_y(Y,Z)) -> every_y(Y, every_x(X,Z))")) : sWeakening.getAllRules();
+        assert ! sWeakening.getAllRules().contains(parseWeakeningRule("[+] every_x(X, every_x(Y,Z)) -> every_x(Y, every_x(X,Z))")) : sWeakening.getAllRules();
+        assert ! sWeakening.getAllRules().contains(parseWeakeningRule("[+] every_y(X, every_y(Y,Z)) -> every_y(Y, every_y(X,Z))")) : sWeakening.getAllRules();
+    }
+
+    @Test
     public void testSpecializingWildcard() throws Exception {
         RewritingSystemParser parser = new RewritingSystemParser();
         RewriteSystem weakening = new RewriteSystem(true);
@@ -85,6 +108,27 @@ class RewriteSystemSpecializerTest {
 
 
     public static final String domgraphWithWildcard = "[label(x pron_rel(x1 x2)) label(y every(y1 y2)) label(w1 foo) label(w2 bar) label(z loves) dom(x1 w1) dom(y1 w2) dom(x2 z) dom(y2 z)]";
+
+    public static String domgraphWithDuplicates = """
+    [label(x every(x1 x2)) label(y every(y1 y2)) label(z loves) label(w1 man) label(w2 woman)
+     dom(x1 w1) dom(y1 w2) dom(x2 z) dom(y2 z)]""";
+
+    public static String testRewritingDummy = """
+
+// weakening rules
+[+] every(X, every(Y,Z)) -> every(Y, every(X,Z))
+
+// annotator
+start annotation: +
+neutral annotation: 0
+
++: a(+,+)
+-: a(-,-)
+
++: every(-,+)
+-: every(+,-)
+
+    """;
 }
 
 
