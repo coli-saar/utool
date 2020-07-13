e1bf44b706480c87215b1a38f2a84aa8663b8a86
Alexander Koller <koller@ling.uni-potsdam.de>
2020-07-13 21:45:39 +0200

    now compiled with Java 12; closes #3

--- src/main/java/de/saar/chorus/ubench/MacIntegration.java
+++ src/main/java/de/saar/chorus/ubench/MacIntegration.java
@@ -1,12 +1,10 @@
 package de.saar.chorus.ubench;
 
-import com.apple.eawt.Application;
-import com.apple.eawt.ApplicationEvent;
-import com.apple.eawt.ApplicationListener;
+import org.madlonkay.desktopsupport.DesktopSupport;
 
 public class MacIntegration {
 	public static boolean isMac() {
-		return System.getProperty("mrj.version") != null;
+		return "Mac OS X".equals(System.getProperty("os.name"));
 	}
 	
 	public static void integrate() {
@@ -14,57 +12,17 @@ public class MacIntegration {
 			System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Utool");
 			System.setProperty("apple.laf.useScreenMenuBar", "true");
 			
-			Application app = Application.getApplication();
+			DesktopSupport.getSupport().setAboutHandler(e -> {
+				Ubench.getInstance().displayAboutDialog();
+			});
 
-			app.addAboutMenuItem();
-			app.setEnabledAboutMenu(true);
-			
-			app.addPreferencesMenuItem();
-			app.setEnabledPreferencesMenu(true);
-			
-			app.addApplicationListener(new UtoolApplicationListener());
-			
+			DesktopSupport.getSupport().setPreferencesHandler(e -> {
+				Ubench.getInstance().setPreferenceDialogVisible(true);
+			});
+
+			DesktopSupport.getSupport().setQuitHandler((e, response) -> {
+				Ubench.getInstance().quit();
+			});
 		}
 	}
-	
-	private static class UtoolApplicationListener implements ApplicationListener {
-		
-		public void handleAbout(ApplicationEvent e) {
-			Ubench.getInstance().displayAboutDialog();
-			e.setHandled(true);
-		}
-
-
-		public void handlePreferences(ApplicationEvent e) {
-			Ubench.getInstance().setPreferenceDialogVisible(true);
-			e.setHandled(true);
-		}
-		
-
-		public void handleQuit(ApplicationEvent e) {
-			Ubench.getInstance().quit();
-			e.setHandled(true);
-		}
-
-		public void handleOpenApplication(ApplicationEvent event) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void handlePrintFile(ApplicationEvent event) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void handleReOpenApplication(ApplicationEvent event) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void handleOpenFile(ApplicationEvent event) {
-			// TODO Auto-generated method stub
-			
-		}
-	}
-	
 }
